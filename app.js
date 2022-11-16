314e83f3c05e8d929e5ebe85f75d0b34e8a5b06e
chachagof <chacha0932@gmail.com>
2022-11-16 20:42:33 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,46 +1,24 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const port = 3000
 // 引用路由器
 const routes = require('./routes')
 
-const port = 3000
+require('./config/mongoose')
+
 const app = express()
 
 // 載入handlebars
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-
 // 載入body-parser
 app.use(bodyParser.urlencoded({ extended: true }))
-
 // 載入method-override
 app.use(methodOverride('_method'))
-
 // 將 request 導入路由器
 app.use(routes)
-
-// 加入這段 code, 僅在非正式環境時, 使用 dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-// 設定連線到 mongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
-
-
 // listen app
 app.listen(port, () => {
   console.log('App is running on http://localhost:3000')
