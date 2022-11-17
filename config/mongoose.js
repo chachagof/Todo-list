4481702e63472d2d1540a5cac6a18dc28af79c81
chachagof <chacha0932@gmail.com>
2022-11-17 22:47:18 +0800

    feat:figure out heroku and update

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,19 +1,14 @@
 const mongoose = require('mongoose')
 
-// 加入這段 code, 僅在非正式環境時, 使用 dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-// 設定連線到 mongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/todo-list'
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
-// 取得資料庫連線狀態
 const db = mongoose.connection
-// 連線異常
+
 db.on('error', () => {
   console.log('mongodb error!')
 })
-// 連線成功
+
 db.once('open', () => {
   console.log('mongodb connected!')
 })
