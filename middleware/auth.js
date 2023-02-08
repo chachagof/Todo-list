1ef45b706729fbfc6c88e76b36a4c220b4ca7c51
chachagof <chacha0932@gmail.com>
2023-02-08 18:32:09 +0800

    fat: add message partials

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,9 @@
 module.exports = {
-  authenticator:(req,res,next)=>{
-    if (req.isAuthenticated()){
+  authenticator: (req, res, next) => {
+    if (req.isAuthenticated()) {
       return next()
     }
+    req.flash('warning_msg', 'Please login first!')
     res.redirect('/users/login')
   }
 }
